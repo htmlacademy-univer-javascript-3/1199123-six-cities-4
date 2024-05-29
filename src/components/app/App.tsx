bb616be2b19453b948854ab1779b9cdd3611e009
Roman Smolentsev <smolentsev.ryu@edu.spbstu.ru>
2024-05-29 15:45:06 +0300

    fix: import issues

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Main } from '../../pages/Main/Main.tsx';
-import { NotFoundPage } from '../../pages/not-found-screen/not-found-screen';
+import { NotFoundPage } from '../../pages/not-found-screen/not-found-screen.tsx';
 import { Offer } from '../../pages/Offer/Offer.tsx';
 import { Login } from '../../pages/Login/Login.tsx';
 import { Favourites } from '../../pages/Favourites/Favourites.tsx';
-import { PrivateRoute } from '../private-route/private-route';
+import { PrivateRoute } from '../private-route/private-route.tsx';
 import { useAppSelector } from '../hooks/index.ts';
-import Spinner from '../../pages/loading-screen/loading-screen';
+import Spinner from '../../pages/loading-screen/loading-screen.tsx';
 
 
 export default function App(): JSX.Element {
